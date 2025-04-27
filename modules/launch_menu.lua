893c989b58113e35f5e0d0ad65df89a7352888e3
Eli <tape-infix.3m@icloud.com>
2025-04-27 00:38:09 -0400

    feat: Move to zsh, fix font, and some formatting

--- modules/launch_menu.lua
+++ modules/launch_menu.lua
@@ -1,103 +1,131 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local launch_menu = {}
 
 table.insert(launch_menu, {
-  label = 'nushell',
-  args = { 'nu' },
+	label = "zsh",
+	args = { "zsh" },
 })
 table.insert(launch_menu, {
-  label = 'nushell (superuser)',
-  args = { 'sudo', 'nu' },
+	label = "nushell",
+	args = { "nu" },
+})
+table.insert(launch_menu, {
+	label = "nushell (superuser)",
+	args = { "sudo", "nu" },
 })
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  table.insert(launch_menu, {
-    label = 'PowerShell 7',
-    args = { 'pwsh', '-NoLogo' },
-  })
-  table.insert(launch_menu, {
-    label = 'PowerShell 7 (superuser)',
-    args = { 'sudo', 'pwsh', '-NoLogo' },
-  })
-  table.insert(launch_menu, {
-    label = 'PowerShell',
-    args = { 'PowerShell', '-NoLogo' },
-  })
-  table.insert(launch_menu, {
-    label = 'cmd',
-    args = { 'cmd', '/k' },
-  })
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	table.insert(launch_menu, {
+		label = "PowerShell 7",
+		args = { "pwsh", "-NoLogo" },
+	})
+	table.insert(launch_menu, {
+		label = "PowerShell 7 (superuser)",
+		args = { "sudo", "pwsh", "-NoLogo" },
+	})
+	table.insert(launch_menu, {
+		label = "PowerShell",
+		args = { "PowerShell", "-NoLogo" },
+	})
+	table.insert(launch_menu, {
+		label = "cmd",
+		args = { "cmd", "/k" },
+	})
 
-  -- top
-  table.insert(launch_menu, {
-    label = 'top',
-    args = { 'ntop' }
-  })
-  table.insert(launch_menu, {
-    label = 'btop',
-    args = { 'btop' }
-  })
+	-- top
+	table.insert(launch_menu, {
+		label = "top",
+		args = { "ntop" },
+	})
+	table.insert(launch_menu, {
+		label = "btop",
+		args = { "btop" },
+	})
 
-  -- VS22
-  table.insert(launch_menu, {
-    label = 'Developer PowerShell for VS 2022',
-    args = { 'pwsh', '-NoExit', '-Command', '&{Import-Module "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 87b72232 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"}' },
-  })
-  table.insert(launch_menu, {
-    label = 'Developer Command Prompt for VS 2022',
-    args = { 'cmd', '/k', 'C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat', '-startdir=none', '-arch=x64', '-host_arch=x64' },
-  })
+	-- VS22
+	table.insert(launch_menu, {
+		label = "Developer PowerShell for VS 2022",
+		args = {
+			"pwsh",
+			"-NoExit",
+			"-Command",
+			'&{Import-Module "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll"; Enter-VsDevShell 87b72232 -SkipAutomaticLocation -DevCmdArguments "-arch=x64 -host_arch=x64"}',
+		},
+	})
+	table.insert(launch_menu, {
+		label = "Developer Command Prompt for VS 2022",
+		args = {
+			"cmd",
+			"/k",
+			"C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\VsDevCmd.bat",
+			"-startdir=none",
+			"-arch=x64",
+			"-host_arch=x64",
+		},
+	})
 
-  -- SSH (for PI)
-  table.insert(launch_menu, {
-    label = 'neTech Raspberry Pi',
-    args = { 'ssh', 'niceeli@100.82.40.144' }
-  })
+	-- SSH (for PI)
+	table.insert(launch_menu, {
+		label = "neTech Raspberry Pi",
+		args = { "ssh", "niceeli@100.82.40.144" },
+	})
 
-  -- UNIX Dev Stuff
-  table.insert(launch_menu, {
-    label = 'Git Bash',
-    args = { 'C:\\Program Files\\Git\\bin\\bash.exe', '-i', '-l' },
-  })
-  table.insert(launch_menu, {
-    label = 'WSL',
-    args = { 'wsl' },
-  })
+	-- UNIX Dev Stuff
+	table.insert(launch_menu, {
+		label = "Git Bash",
+		args = { "C:\\Program Files\\Git\\bin\\bash.exe", "-i", "-l" },
+	})
+	table.insert(launch_menu, {
+		label = "WSL",
+		args = { "wsl" },
+	})
 
-  -- Python
-  table.insert(launch_menu, {
-    label = 'Anaconda PowerShell Prompt (24.9.2-0)',
-    args = { 'powershell.exe', '-ExecutionPolicy', 'ByPass', '-NoExit', '-Command', "& 'C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0\\shell\\condabin\\conda-hook.ps1' ; conda activate 'C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0'" },
-  })
-  table.insert(launch_menu, {
-    label = 'Anaconda Prompt (24.9.2-0)',
-    args = { 'cmd.exe', '/K', 'C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0\\Scripts\\activate.bat', 'C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0' },
-  })
+	-- Python
+	table.insert(launch_menu, {
+		label = "Anaconda PowerShell Prompt (24.9.2-0)",
+		args = {
+			"powershell.exe",
+			"-ExecutionPolicy",
+			"ByPass",
+			"-NoExit",
+			"-Command",
+			"& 'C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0\\shell\\condabin\\conda-hook.ps1' ; conda activate 'C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0'",
+		},
+	})
+	table.insert(launch_menu, {
+		label = "Anaconda Prompt (24.9.2-0)",
+		args = {
+			"cmd.exe",
+			"/K",
+			"C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0\\Scripts\\activate.bat",
+			"C:\\Users\\niceEli\\scoop\\apps\\miniconda3\\24.9.2-0",
+		},
+	})
 end
 
 if wezterm.target_triple ~= "x86_64-pc-windows-msvc" then
-  table.insert(launch_menu, {
-    label = 'zsh',
-    args = { 'zsh' },
-  })
-  table.insert(launch_menu, {
-    label = 'zsh (superuser)',
-    args = { 'sudo', 'zsh' },
-  })
-  table.insert(launch_menu, {
-    label = 'bash',
-    args = { 'bash' },
-  })
+	table.insert(launch_menu, {
+		label = "zsh",
+		args = { "zsh" },
+	})
+	table.insert(launch_menu, {
+		label = "zsh (superuser)",
+		args = { "sudo", "zsh" },
+	})
+	table.insert(launch_menu, {
+		label = "bash",
+		args = { "bash" },
+	})
 
-  -- top
-  table.insert(launch_menu, {
-    label = 'top',
-    args = { 'htop' }
-  })
-  table.insert(launch_menu, {
-    label = 'btop',
-    args = { 'btop' }
-  })
+	-- top
+	table.insert(launch_menu, {
+		label = "top",
+		args = { "htop" },
+	})
+	table.insert(launch_menu, {
+		label = "btop",
+		args = { "btop" },
+	})
 end
 
 return launch_menu
--- wezterm.lua
+++ wezterm.lua
@@ -1,58 +1,66 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
-local launch_menu = require 'modules/launch_menu'
+local wezterm = require("wezterm")
+local launch_menu = require("modules/launch_menu")
 
 local config = wezterm.config_builder()
 
 -- MAKE 100% SURE TO MAKE shell_integration.osc133 TO FALSE IN $nu.config-path
 
 config = {
-  -- Shell configuration
-  default_prog = { "nu" },
+	-- Shell configuration
+	default_prog = { "zsh" },
 
-  -- Font configuration
-  font = wezterm.font_with_fallback {
-    {
-      family = 'JetBrainsMono NFM Medium',
-      weight = 'Medium',
-    },
-    { family = 'Terminus', weight = 'Bold' },
-    'Noto Color Emoji',
-  },
-  font_size = 14.0,
+	-- Font configuration
+	font = wezterm.font_with_fallback({
+		--{
+		--	family = "JetBrainsMono NFM Medium",
+		--	weight = "Bold",
+		--},
+		--	{ family = "Terminus", weight = "Bold" },
+		{ family = "JetBrains Mono", weight = 'Bold' },
+		"Symbols Nerd Font",
+		"Noto Color Emoji",
+	}),
+	font_size = 12.0,
 
-  -- Window configuration
-  initial_rows = 32,
-  initial_cols = 120,
-  window_padding = {
-    left = 6,
-    right = 6,
-    top = 6,
-    bottom = 6,
-  },
-  window_background_opacity = 0.95,
-  use_resize_increments = true,
+	-- Window configuration
+	initial_rows = 42,
+	initial_cols = 140,
+	-- window_padding = {
+	--  left = 6,
+	--  right = 6,
+	--  top = 6,
+	--  bottom = 6,
+	--},
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+	window_background_opacity = 0.95,
+	use_resize_increments = true,
 
-  -- UI Elements
-  use_fancy_tab_bar = false,
-  tab_bar_at_bottom = true,
-  enable_scroll_bar = true,
+	-- UI Elements
+	use_fancy_tab_bar = false,
+	tab_bar_at_bottom = true,
+	enable_scroll_bar = true,
 
-  -- Visual and sound settings
-  color_scheme = 'Vice Dark (base16)',
-  colors = {
-    visual_bell = '#202020',
-  },
-  visual_bell = {
-    fade_out_function = 'EaseOut',
-    fade_out_duration_ms = 150,
-  },
-  audible_bell = 'Disabled',
+	-- Visual and sound settings
+	color_scheme = "Vice Dark (base16)",
+	colors = {
+		visual_bell = "#202020",
+	},
+	visual_bell = {
+		fade_out_function = "EaseOut",
+		fade_out_duration_ms = 150,
+	},
+	audible_bell = "Disabled",
 
-  -- Input settings
-  enable_kitty_keyboard = true,
+	-- Input settings
+	enable_kitty_keyboard = true,
 
-  launch_menu = launch_menu,
+	launch_menu = launch_menu,
 }
 
 return config
